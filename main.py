0b10e503f3f40c7db1e4bc663a3c24ff28ee10c5
Reo Nishida <leo.nishida1030@gmail.com>
2026-02-15 21:33:18 +0900

    feat: SQLAlchemyを導入して、SQLite接続とSessionのDIを追加

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
+
+DATABASE_URL = "sqlite:///./app.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db() -> Generator[Session,None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI(title="FastAPI CRUD - Tasks")
 
 @app.get("/health")
-def health():
+def health(db: Session = Depends(get_db)):
     return {"ok": True}
